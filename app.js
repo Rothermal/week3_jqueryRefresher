939619ac38c1eec7705d93f76867811690be8471
Mark Rothermal <rothermal2@comcast.net>
2016-01-04 13:19:24 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/**
+ * Created by mark on 1/4/16.
+ */
+console.log("test");
+$(document).ready(function(){
+    init();
+});
+
+function init () {
+    console.log("jquery is great success");
+}
+
+    $.ajax('data.json').then(function (response) {
+
+        for (var i = 0; i < response.length; i++) {
+            var $el = '';
+
+            var id = response[i].id;
+            var gender = response[i].gender;
+            var first_name = response[i].first_name;
+            var last_name = response[i].last_name;
+            var email = response[i].email;
+
+            //console.log(id, gender, first_name, last_name, email);
+           var $content = '<br/>'+'Id: '+ id + '<br/>' + 'Gender: '+ gender +'<br/>' + 'First name: ' + first_name + '<br/>'+ 'Last name: ' + last_name + '<br/>' + 'Email: '+ email + '<br/>';
+
+         $('.people').append($content);
+
+
+
+
+
+        }
+
+    });
+

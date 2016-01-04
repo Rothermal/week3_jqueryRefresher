615260a033c14e74d2aea40b0d8de093ecc9f442
Mark Rothermal <rothermal2@comcast.net>
2016-01-04 13:58:14 -0600

    DRY

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ console.log("test");
 $(document).ready(function(){
     init();
 });
-
 function init () {
     console.log("jquery is great success");
 }
-
     $.ajax('data.json').then(function (response) {
-
         for (var i = 0; i < response.length; i++) {
             var $content = '';
 
@@ -24,13 +21,6 @@ function init () {
             //console.log(id, gender, first_name, last_name, email);
          $content = 'Id: '+ id + '<br/>' + 'Gender: '+ gender +'<br/>' + 'First name: ' + first_name + '<br/>'+ 'Last name: ' + last_name + '<br/>' + 'Email: '+ email + '<br/>';
 
-         $('.people').append('<p>'+$content+'</p>');
-
-
-
-
-
+         $('.people').append('<p>'+$content+'</p>').hide().fadeIn();
         }
-
     });
-
